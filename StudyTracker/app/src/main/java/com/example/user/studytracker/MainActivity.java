7360511f8b59e54709a23c461007648863eac0ef
fheinrich29 <florianheinrich9999@gmail.com>
2017-11-30 14:46:13 +0100

    moved settings to actionbar

--- StudyTracker/app/src/main/java/com/example/user/studytracker/MainActivity.java
+++ StudyTracker/app/src/main/java/com/example/user/studytracker/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.user.studytracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,19 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSettings = (Button) findViewById(R.id.buttonSettings);
-
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToSettings(v);
-            }
-        });
     }
 
-    public void goToSettings(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
